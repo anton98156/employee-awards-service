aa56193f76e22a4786a73487303beea7d0444ea4
anton <anton98156789@gmail.com>
2025-11-02 12:00:33 +0300

    Add file DTO

---
+++ src/main/java/ru/t2/employeeawards/DTO/AwardFileRecord.java
@@ -0,0 +1,31 @@
+package ru.t2.employeeawards.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AwardFileRecord {
+
+    @NotNull
+    private Long employeeExternalId;
+
+    @NotBlank
+    private String employeeFullName;
+
+    @NotNull
+    private Long awardExternalId;
+
+    @NotBlank
+    private String awardName;
+
+    @NotNull
+    private LocalDate receivedDate;
+}
